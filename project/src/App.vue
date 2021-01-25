81ab33656591df5fd09e243cae817b8510bf6fb5
harili <ilieschafyfy@gmail.com>
2021-01-25 21:01:55 +0100

    Feature : Add some object and not using Axios for test

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+        <h2> Contrôle de la qualité de l'air</h2> 
+        <div class="row">
+          <div class="col-sm-4" v-for="city of cities" :key="city.index">
+              <City :city="city" />
+          </div>
+        </div>
+          
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import City from '@/components/City'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    City
+  },
+  data() {
+    return {
+      cities: [{name:"Lyon", iqa:30},
+              {name:"Paris", iqa:21},
+              {name:"Orléans", iqa:12}]
+    }
+  },
 }
 </script>
 
---
+++ project/src/components/City.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div>
+  <b-card
+    :title="city.name"
+  >
+    <b-card-text >
+      Qualité de l'air : {{city.iqa}}
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            city:{
+                type:Object,
+                default:null
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
